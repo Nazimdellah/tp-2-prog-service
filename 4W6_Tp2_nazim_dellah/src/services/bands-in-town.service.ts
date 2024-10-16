ddeea590682a727fa4a7935c0ca735de7396b1ed
nazim <2378684@cegepmontpetit.ca>
2024-10-16 15:16:50 -0400

    tp finit

--- 4W6_Tp2_nazim_dellah/src/services/bands-in-town.service.ts
+++ 4W6_Tp2_nazim_dellah/src/services/bands-in-town.service.ts
@@ -6,7 +6,7 @@ import { Album } from '../models/Album';
 import { Song } from '../models/Song'; // Import Song class
 import { Show } from '../models/show';
 
-const key : string = "2b32475766802ac01eefda45e9e42ea0";
+const key: string = "2b32475766802ac01eefda45e9e42ea0";
 @Injectable({
   providedIn: 'root'
 
@@ -16,15 +16,20 @@ export class BandsInTownService {
 
 
 
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient) { }
 
   async ngOnInit() {
 
   }
-  async getShow(artist: string): Promise<void> {
+  async getShow(artist: string): Promise<Show[]> {
     const url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${key}`;
     let x = await lastValueFrom(this.http.get<any>(url));
-    console.log(x);
+    console.log(x); let shows: Show[] = [];
+    for (let i = 0; i < x.length; i++) {
+      shows.push(new Show(x[i].venue.latitude, x[i].venue.longitude, x[i].venue.country, x[i].venue.city, x[i].datetime));
+    }
+
+    return shows
 
   }
 }
--- 4W6_Tp2_nazim_dellah/src/show/show.component.ts
+++ 4W6_Tp2_nazim_dellah/src/show/show.component.ts
@@ -1,15 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
+import { Show } from '../models/show';
+import { HttpClient } from '@angular/common/http';
+import { BandsInTownService } from '../services/bands-in-town.service';
+
 
 @Component({
   selector: 'app-show',
+  standalone: true,
+  imports: [FormsModule, RouterModule, RouterOutlet, CommonModule],
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
 export class ShowComponent implements OnInit {
+  artistName: string | null = null
 
-  constructor() { }
+  shows: Show[] = []
+
+  constructor(public route: ActivatedRoute, public http: HttpClient, private bandsInTownService: BandsInTownService) { }
 
   ngOnInit() {
-  }
+    this.artistName = this.route.snapshot.paramMap.get("artistName")
+    if (this.artistName) {
+      this.getShows(this.artistName);
+    }
 
+
+  }
+  async getShows(artistName: string): Promise<void> {
+    try {
+      this.shows = await this.bandsInTownService.getShow(artistName);
+      if (this.shows.length == 0) {
+        console.log('No events found for this artist.');
+      }
+      else {
+        console.log('Events found for this artist:', this.shows);
+      }
+    }
+    catch (error) {
+      console.error('An error occurred while trying to get events for this artist:', error);
+    }
+  }
 }
+
+
